0d34f2afa2fa89b7c8fca40947381781b904273f
the-terror-wonder <akpanchayan@gmail.com>
2025-07-20 03:06:13 +0530

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="absolute top-0 left-0 right-0 z-20">
+        <header className="fixed top-0 left-0 right-0 z-20 bg-slate-900/80 backdrop-blur-sm">
             <nav className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-20">
                     <div className="flex-shrink-0">
